0799bd356afdc9579411480e893d8d1bf071a437
Junaid Babatunde <junaidtunde1@gmail.com>
2020-02-03 10:56:29 +0100

    pushing for deployment

--- client/src/config/server.js
+++ client/src/config/server.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'http://localhost:3000/api/v1';
+// export const baseUrl = 'http://localhost:3000/api/v1';
+export const baseUrl = "https://jetcake-react.herokuapp.com/api/v1";
